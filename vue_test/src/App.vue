71f107b2096accfdfffb256ff23277c98f9f5725
yxs <y_xs@winning.com.cn>
2021-11-09 22:26:20 +0800

    vue props属性学习

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,29 +1,14 @@
 <template>
 	<div>
-		<h1 v-text="msg" ref="title"></h1>
-		<button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
-		<School ref="sch"/>
+		<Student name="李四" sex="女" :age="18"/>
 	</div>
 </template>
 
 <script>
-	//引入School组件
-	import School from './components/School'
+	import Student from './components/Student'
 
 	export default {
-		name: 'App',
-		components: {School},
-		data() {
-			return {
-				msg: '欢迎学习Vue！'
-			}
-		},
-		methods: {
-			showDOM() {
-				console.log(this.$refs.title) //真实DOM元素
-				console.log(this.$refs.btn) //真实DOM元素
-				console.log(this.$refs.sch) //School组件的实例对象（vc）
-			}
-		}
+		name:'App',
+		components:{Student}
 	}
 </script>
